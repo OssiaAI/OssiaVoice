8473dd3c2e3911bacbc439c05a38eeb8591de209
James Arney <james@biocortex.com>
2024-03-20 23:43:32 +0000

    attempting to fix url path

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/OccamVoice/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
