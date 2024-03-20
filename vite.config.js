75de1f0f6406d9f5db1f451c0f9363829321a813
James Arney <james@biocortex.com>
2024-03-20 23:46:17 +0000

    attempting to fix url path (2)

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/',
+  base: '/OssiaVoice/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
