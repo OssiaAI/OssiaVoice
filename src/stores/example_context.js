71cb83bd42b89dcfbe3e46e052b2abd62353df0a
James Arney <james@biocortex.com>
2024-03-24 20:34:01 +0000

    tweaking example context

--- src/stores/example_context.js
+++ src/stores/example_context.js
@@ -1,3 +1,3 @@
-export const exampleContext= `Current Time: 20:37
-Current Date: Monday 25th December 2023
-Geographic Region: Glasgow`;
+export const exampleContext= `Time: 20:37
+Date: Monday 25 December 2023
+Location: Glasgow`;
